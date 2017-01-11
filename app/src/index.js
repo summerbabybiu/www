1d22ef3cb88842bb4d3810057b725f5684c51ac0
stephenw <zhilong.wang@ele.me>
2017-01-11 10:49:28 +0800

    add sdk initialize

--- app/src/index.js
+++ app/src/index.js
@@ -1,12 +1,15 @@
 var express = require('express'),
     app = express();
 
+//初始化数据SDK
+require('./models').initdb();
 //添加路由
 require('./routes')(app);
 
 if (module.parent) {
     module.exports = app;
 } else {
+    //docker运行，具体物理端口由compose设定
     app.listen(3000, () => {
         console.log('app is running on port 3000');
     });
--- app/src/models/index.js
+++ app/src/models/index.js
@@ -1,6 +1,7 @@
 var Parse = require('parse/node'),
-    config = require('../config');
+    parse_config = require('../config').parse;
 
 exports.initdb = function() {
-    
+    Parse.initialize(parse_config.app_id);
+    Parse.serverURL = parse_config.server_url;
 }
